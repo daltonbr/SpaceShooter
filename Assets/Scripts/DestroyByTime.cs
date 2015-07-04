76308334739611050aa3e34df2239e8fc225cafa
daltonbr <sacinopatinete@gmail.com>
2015-07-04 14:34:07 -0300

    Spawning Waves

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float lifetime; // this is the time of the gameObject
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,15 +5,29 @@ public class GameController : MonoBehaviour {
 
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hazardCount;  // total of hazards per Wave
+    public float spawnWait;  // time between Spawns
+    public float startWait;  // initial time before start a wave
+    public float waveWait;   //  time between waves
 
     void Start()
     {
-        SpawnWaves();
+        StartCoroutine ( SpawnWaves() );   //the way to call a Coroutine
     }
-    void SpawnWaves () //spawn a single Asteroid in a random position
+
+    IEnumerator SpawnWaves ()  // to WaitForSeconds we need this function to be iEnumerator
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, +spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;  //no rotation at Spawning
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(startWait);  // initial time before start the wave
+        while (true)    // endless loop
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, +spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;  //no rotation at Spawning
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait); // start a coroutine that pause this olny this function
+            }
+            yield return new WaitForSeconds(waveWait);  // time between waves
+        }
     }
 }
