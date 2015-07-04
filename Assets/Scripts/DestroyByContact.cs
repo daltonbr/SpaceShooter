8599026d03ea3d1b0cc0e70dd9c58e591990a858
daltonbr <sacinopatinete@gmail.com>
2015-07-04 11:32:06 -0300

    Game Controller

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);  //instantiate the explosion of the Asteroid
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);  //instantiate the explosion of the Player
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
