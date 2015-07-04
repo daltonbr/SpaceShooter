db2ef848819e095f9a5818b43e52ba466bca4af2
daltonbr <sacinopatinete@gmail.com>
2015-07-04 11:51:44 -0300

    Game Controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+    void Start()
+    {
+        SpawnWaves();
+    }
+    void SpawnWaves () //spawn a single Asteroid in a random position
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, +spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;  //no rotation at Spawning
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
